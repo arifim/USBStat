172cc70cf8e243c5fbb6ebd23549e3eaf9f90756
arif rakhmanov <arif.rakhmanov@gmail.com>
2026-06-22 21:12:26 -0400

    fix: right click on app icon open menu to close the app

--- USBStat/USBStatApp.swift
+++ USBStat/USBStatApp.swift
@@ -1,20 +1,71 @@
 //Arif Rakhmanov 6/21/26
 
 import SwiftUI
+import AppKit
 
 @main
 struct USBStatApp: App {
-    @StateObject private var viewModel = USBViewModel()
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
 
     var body: some Scene {
-        MenuBarExtra {
-            ContentView()
-                .environmentObject(viewModel)
-                .frame(width: 380, height: 480)
-        } label: {
-            Image("trayIcon")
-                .renderingMode(.template)
+        Settings { EmptyView() }
+    }
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    private var statusItem: NSStatusItem!
+    private var popover: NSPopover!
+    private let viewModel = USBViewModel()
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        NSApp.setActivationPolicy(.accessory)
+
+        popover = NSPopover()
+        popover.contentSize = NSSize(width: 360, height: 480)
+        popover.behavior = .transient
+        popover.contentViewController = NSHostingController(
+            rootView: ContentView().environmentObject(viewModel)
+        )
+
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        if let button = statusItem.button {
+            let image = NSImage(named: "trayIcon")
+            image?.isTemplate = true
+            button.image = image
+            button.target = self
+            button.action = #selector(handleClick(_:))
+            button.sendAction(on: [.leftMouseUp, .rightMouseUp])
         }
-        .menuBarExtraStyle(.window)
+    }
+
+    @objc private func handleClick(_ sender: NSStatusBarButton) {
+        guard let event = NSApp.currentEvent else { return }
+        if event.type == .rightMouseUp {
+            showContextMenu()
+        } else {
+            togglePopover()
+        }
+    }
+
+    private func togglePopover() {
+        guard let button = statusItem.button else { return }
+        if popover.isShown {
+            popover.performClose(nil)
+        } else {
+            popover.show(relativeTo: button.bounds, of: button, preferredEdge: .minY)
+            popover.contentViewController?.view.window?.makeKey()
+        }
+    }
+
+    private func showContextMenu() {
+        let menu = NSMenu()
+        menu.addItem(NSMenuItem(title: "Quit", action: #selector(quit), keyEquivalent: "q"))
+        statusItem.menu = menu
+        statusItem.button?.performClick(nil)
+        statusItem.menu = nil
+    }
+
+    @objc private func quit() {
+        NSApp.terminate(nil)
     }
 }
